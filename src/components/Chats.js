8c6c774606af09c004f895af1aea3d459e226b1c
Krzysztof Zbrojkiewicz <chris.z@aol.com>
2021-12-15 20:41:42 +0000

    fix typo

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -69,7 +69,7 @@ const Chats = () => {
         </div>
       </div>
       <ChatEngine
-        hight="calc(100vh - 66px)"
+        height="calc(100vh - 66px)"
         projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
         userName={user.email}
         userSecret={user.uid}
